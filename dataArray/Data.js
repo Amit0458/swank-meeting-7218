fc8141cd2512f7e0529f6493871c91ee945b8524
Amit0458 <amitkumar51997@gmail.com>
2022-11-09 23:43:49 +0530

    day-2 issues and tasks

---
+++ dataArray/Data.js
@@ -0,0 +1,237 @@
+/*********************************************************************Home Page*******************************************************************/
+
+mainPage = [
+    navBar = {
+        companyLogo: "https://assets.interntheory.com/creative/logo.png"
+    },
+    bannerSection = {
+        heading: "Let’s Get You Ready",
+        paragraph: "The One Stop to Upgrade Your Resume. Expand Your Skillset. Do An Internship. Get Placed",
+        bannerBackgroundImg: "https://assets.interntheory.com/creative/Homepage-Banner.png"
+    },
+
+]
+
+FrontPageSection = [
+    TopOpporTunitiesSection = {
+
+        "heading": "PLETHORA OF OPPORTUNITIES FOR YOU",
+        "paragraph": "We are on a mission to connect employers to young talent all over India. With our strong corporate network, it will no longer be a challenge to find the right internship for you",
+        img: {
+            disney: "https://assets.interntheory.com/creative/home-logo/logo-1.png",
+            milward: "https://assets.interntheory.com/creative/home-logo/logo-3.png",
+            maxHealthcare: "https://assets.interntheory.com/creative/home-logo/logo-4.png",
+            weChat: "https://assets.interntheory.com/creative/home-logo/logo-6.png",
+            Microsoft: "https://assets.interntheory.com/creative/home-logo/logo-7.png",
+            lic: "https://assets.interntheory.com/creative/home-logo/logo-8.png",
+            porsche: "https://assets.interntheory.com/creative/home-logo/logo-9.png",
+            fever104: "https://assets.interntheory.com/creative/home-logo/logo-10.png",
+            bookMyShow: "https://assets.interntheory.com/creative/home-logo/logo-11.png",
+            oyo: "https://assets.interntheory.com/creative/home-logo/logo-13.png",
+            itc: "https://assets.interntheory.com/creative/home-logo/logo-14.png",
+            directi: "https://assets.interntheory.com/creative/home-logo/logo-15.png",
+            reliance: "https://assets.interntheory.com/creative/home-logo/logo-16.png",
+            jio: "https://assets.interntheory.com/creative/home-logo/logo-17.png",
+            kotak: "https://assets.interntheory.com/creative/home-logo/logo-18.png",
+            swiggy: "https://assets.interntheory.com/creative/home-logo/logo-19.png",
+            starPlus: "https://assets.interntheory.com/creative/home-logo/logo-20.png",
+            viacom18: "https://assets.interntheory.com/creative/home-logo/logo-21.png"
+        }
+    },
+
+    midSection = {
+        heading: "LET’S GET SOME WORK EXPERIENCE",
+        marketing: "https://assets.interntheory.com/creative/home-internships/Marketing-Icon.jpg",
+        businessDevelopment: "https://assets.interntheory.com/creative/home-internships/BD.jpg",
+        contentWriting: "https://assets.interntheory.com/creative/home-internships/Content-Writing.jpg",
+        graphicDesign: "https://assets.interntheory.com/creative/home-internships/Graphic.jpg",
+        finance: "https://assets.interntheory.com/creative/home-internships/Finance.jpg",
+        humanResource: "https://assets.interntheory.com/creative/home-internships/HR.jpg",
+        engineering: "https://assets.interntheory.com/creative/home-internships/Eng.jpg",
+        allInternships: "https://assets.interntheory.com/creative/home-internships/All-Internships.jpg"
+    },
+
+    SectionBottomSction = {
+        heading: "HOW ABOUT UPSKILLING ?",
+        stockTrade: "https://assets.interntheory.com/creative/home-courses/Homepage-stock.jpg",
+        digitalMaketing: "https://assets.interntheory.com/creative/home-courses/Homepage-digital.jpg",
+        makeOne: "https://assets.interntheory.com/creative/home-courses/Homepage-web.jpg"
+    },
+
+    bottomChartData = {
+        image: "https://assets.interntheory.com/creative/chart.png",
+        heading: "WE ARE CONSTANTLY GROWING",
+        paragraph: "We are on a mission to help every student in India. Upgrading your CV and getting you a meaningful job remains our sole motive and we are not stopping anytime soon."
+    }
+]
+
+mainPageReviewSectionSlidingArea = [
+    amaniNagda = {
+        "img": "https://assets.interntheory.com/img/testimonial-image/in3.jpg",
+        "name": "Amani Nagda",
+        "description": "I was on a look out for an internship to spend my summer productively and I stumbled upon Inter Theory. Their team was very kind and kept me updated. I applied for Viacom18 and got through. I thank Intern Theory team for giving me this opportunity.",
+        "job": "Interrn at Viacom18"
+
+    },
+    siddharthViyappu = {
+        "img": "https://assets.interntheory.com/img/testimonial-image/in2.jpg",
+        "name": "Siddharth Viyappu",
+        "description": "I created my profile because I had before Intern Theory, not come across a consultancy that would execute the responsibility of getting a result for your application irrespective of being failure or success the way they did. They've set the bars really high! Kudos team!",
+        "job": "Intern at Gooseberry Homes"
+
+    },
+    siddharthViyappu = {
+        "img": "https://assets.interntheory.com/img/testimonial-image/in1.jpg",
+        "name": "Sshikha Bodwaani",
+        "description": "We have been using InternTheory for over a year now and are very happy with the quality of applications that we receive. Their team has been very helpful in screening applications for the various internship requirements we had. It has been a delight working with them.",
+        "job": "Sr. Exec. HR at BookMyShow"
+
+    },
+    harshilBhadra = {
+        "img": "https://assets.interntheory.com/img/testimonial-image/in4.jpg",
+        "name": "Harshil Bhadra",
+        "description": "If you are more than satisfied with a particular product/firm who gives you more than you expect then there’s no need to find a substitute. Its similar with Intern Theory, the best team who makes continuous efforts and always ready to help! Thanks for giving me the best start.",
+        "job": "Intern at Porsche"
+    }
+
+]
+/******************************************************sign in page********************************************************/
+background_img = "https://assets.interntheory.com/creative/Web-Banner.jpg";
+/*****************************************************Course Page**********************************************************/
+
+courseSectionBannerTopBanner = [
+    background_img = {
+        "background_img": "https://assets.interntheory.com/creative/courses/listpage/Course-bannerbg.jpg",
+    },
+
+    topText = {
+        "top-text": "https://assets.interntheory.com/creative/courses/listpage/Course-banner1.png"
+    }
+]
+heading = "Certified Online Courses";
+paragraph = "Upgrade your skills with Intern Theory's online learning platform. Enrol for any certified online courses and get a step ahead of competition. Select from a range of skill based online courses and kickstart your career.";
+courseSectionMidBox = [
+    img = [
+        "https://assets.interntheory.com/creative/courses/thumbnails/it.jpg",
+        "https://assets.interntheory.com/creative/courses/thumbnails/digital-marketing-course.png",
+        "https://assets.interntheory.com/creative/courses/thumbnails/data-analytics-r-programming-course.png",
+        "https://assets.interntheory.com/creative/courses/thumbnails/web-development-course.png",
+        "https://assets.interntheory.com/creative/courses/thumbnails/tally-gst-course.png",
+        "https://assets.interntheory.com/creative/courses/thumbnails/android-app-development-kotlin-course.png",
+        "https://assets.interntheory.com/creative/courses/thumbnails/advanced-excel-course.png",
+        "https://assets.interntheory.com/creative/courses/thumbnails/r-programming-course.png",
+    ]
+]
+/*************************************************  Register Page / comapny  *************************************************************************/
+heading = "17082 Companies";
+paragraph = "Trust Us With Their Intern Requirements Wouldn't You?";
+colorImage = {
+    disney: "https://assets.interntheory.com/creative/home-logo/logo-1.png",
+    milward: "https://assets.interntheory.com/creative/home-logo-colored/logo-3.png",
+    esselVission: "https://assets.interntheory.com/creative/home-logo-colored/logo-2.png",
+    maxHealthcare: "https://assets.interntheory.com/creative/home-logo-colored/logo-5.png",
+    weChat: "https://assets.interntheory.com/creative/home-logo-colored/logo-6.png",
+    Microsoft: "https://assets.interntheory.com/creative/home-logo-colored/logo-7.png",
+    lic: "https://assets.interntheory.com/creative/home-logo-colored/logo-8.png",
+    porsche: "https://assets.interntheory.com/creative/home-logo-colored/logo-9.png",
+    fever104: "https://assets.interntheory.com/creative/home-logo-colored/logo-10.png",
+    bookMyShow: "https://assets.interntheory.com/creative/home-logo-colored/logo-11.png",
+    oyo: "https://assets.interntheory.com/creative/home-logo-colored/logo-13.png",
+    itc: "https://assets.interntheory.com/creative/home-logo-colored/logo-14.png",
+    directi: "https://assets.interntheory.com/creative/home-logo-colored/logo-15.png",
+    reliance: "https://assets.interntheory.com/creative/home-logo-colored/logo-17.png",
+    jio: "https://assets.interntheory.com/creative/home-logo-colored/logo-17.png",
+    kotak: "https://assets.interntheory.com/creative/home-logo/logo-18.png",
+    swiggy: "https://assets.interntheory.com/creative/home-logo-colored/logo-18.png",
+    starPlus: "https://assets.interntheory.com/creative/home-logo-colored/logo-20.png",
+    viacom18: "https://assets.interntheory.com/creative/home-logo-colored/logo-4.png"
+}
+midChartData = {
+    image: "https://assets.interntheory.com/creative/chart.png",
+    heading: "WE ARE CONSTANTLY GROWING",
+    paragraph: "We are on a mission to help every student in India. Upgrading your CV and getting you a meaningful job remains our sole motive and we are not stopping anytime soon."
+};
+
+midBottomData = {
+    firstBox: {
+        image: "https://assets.interntheory.com/creative/stepIcons/sign-up.png",
+        heading: "Sign Up",
+        paragraph: "Create an account on InternTheory by filling the form on the right.",
+    },
+    secondBox: {
+        image: "https://assets.interntheory.com/creative/stepIcons/Post-Internship.png",
+        heading: "Post An Internship For Free",
+        paragraph: "Share details on the kind of intern you are looking for by filling our internship form.",
+    },
+    thirdBox: {
+        image: "https://assets.interntheory.com/creative/stepIcons/Notification.png",
+        heading: "Receive Notifications on Email",
+        paragraph: "Once your internship is verified, the relevant students and placement networks will be notified via our match-making algorithms and Pan-India network",
+    },
+    fourthBox: {
+        image: "https://assets.interntheory.com/creative/stepIcons/Review,shortlist&-hire.png",
+        heading: "Review Profile, Shortlist & Hire!",
+        paragraph: "Students with matching eligibility and availability will apply. You can review their profile on your dashboard, and get in touch with the one you like!",
+    }
+};
+
+
+bottomLastBox = {
+    heading: "Other Ways In Which We Can Help :)",
+    paragraph: "Apart from catering as a free platform to post your internship and hire interns, we also provide an array of other services that may interest you :) !",
+    firstBox: {
+        img: "https://assets.interntheory.com/creative/stepIcons/Annual-Intern.png",
+        heading: 'Annual Intern Recruitment',
+        paragraph: "If you are an organization that works extensively with interns and needs them all round the year, we offer specialized solutions to make your job easier and faster!"
+    },
+    secondBox: {
+        img: "https://assets.interntheory.com/creative/stepIcons/Junior-Employee.png",
+        heading: 'Annual Intern Recruitment',
+        midHeading: "(For Mumbai & Delhi Only)",
+        para: "Apart from interns, if you are also looking to hire full time job candidates in the 0-3 years experience range, you may want to talk to our specialized hiring team :)"
+    },
+    thirdBox: {
+        img: "https://assets.interntheory.com/creative/stepIcons/Youth-Marketing.png",
+        heading: 'Youth Marketing & Campus Activations',
+        paragraph: "With a solid Pan-India Campus ambassador Network and a reach of more than 5 Lakh students and 2000 colleges, we help companies who wish to run student marketing campaigns among the 18-24 age group."
+    }
+}
+
+
+lastBox = {
+    heading: "What Others Say About Us",
+    para: "Some good people have some good things to say about us! We hope you feel the same after your experience with InternTheory :)",
+    ReviewSectionSlidingArea: {
+        amaniNagda: {
+            "img": "https://assets.interntheory.com/img/testimonial-image/in3.jpg",
+            "name": "Amani Nagda",
+            "description": "I was on a look out for an internship to spend my summer productively and I stumbled upon Inter Theory. Their team was very kind and kept me updated. I applied for Viacom18 and got through. I thank Intern Theory team for giving me this opportunity.",
+            "job": "Interrn at Viacom18"
+
+        },
+        siddharthViyappu: {
+            "img": "https://assets.interntheory.com/img/testimonial-image/in2.jpg",
+            "name": "Siddharth Viyappu",
+            "description": "I created my profile because I had before Intern Theory, not come across a consultancy that would execute the responsibility of getting a result for your application irrespective of being failure or success the way they did. They've set the bars really high! Kudos team!",
+            "job": "Intern at Gooseberry Homes"
+
+        },
+        siddharthViyappu: {
+            "img": "https://assets.interntheory.com/img/testimonial-image/in1.jpg",
+            "name": "Sshikha Bodwaani",
+            "description": "We have been using InternTheory for over a year now and are very happy with the quality of applications that we receive. Their team has been very helpful in screening applications for the various internship requirements we had. It has been a delight working with them.",
+            "job": "Sr. Exec. HR at BookMyShow"
+
+        },
+        harshilBhadra: {
+            "img": "https://assets.interntheory.com/img/testimonial-image/in4.jpg",
+            "name": "Harshil Bhadra",
+            "description": "If you are more than satisfied with a particular product/firm who gives you more than you expect then there’s no need to find a substitute. Its similar with Intern Theory, the best team who makes continuous efforts and always ready to help! Thanks for giving me the best start.",
+            "job": "Intern at Porsche"
+        }
+
+    }
+
+}
+/****************************************************Register Student*************************************************************************/
+background_img = "https://assets.interntheory.com/creative/Web-Banner.jpg";
